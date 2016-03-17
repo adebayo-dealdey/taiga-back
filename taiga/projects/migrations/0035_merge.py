c9deb88265ed1d23568cadb7540378fa1ca6838b
Akinlaja Adebayo <adebayo@dealdey.com>
2016-03-17 16:10:22 +0100

    Automatic merge created for migration

---
+++ taiga/projects/migrations/0035_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0028_auto_20160117_1548'),
+        ('projects', '0034_project_looking_for_people_note'),
+    ]
+
+    operations = [
+    ]
